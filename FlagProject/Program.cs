a0da25af3ce25d129c005f54fe8d41c30f4c4759
Piotr Wątorski <piotr.watorski@gmail.com>
2019-12-21 16:26:45 +0100

    updated biases applied

--- FlagProject/Program.cs
+++ FlagProject/Program.cs
@@ -15,14 +15,15 @@ namespace FlagProject
     class Program
     {
         static Vector<double>[] flagMatrixes;
-        static int pWidth = 12;
-        static int pHeight = 8;
+        static int pWidth = 24;
+        static int pHeight = 16;
         static int pTotal = pWidth * pHeight;
-        static int memoryPoint;
+        static int memoryPoint = 0;
         static Int2 displaySize;
         static float[] memory;
         static List<float> memoryLong;
-        static float maxErr = 1;
+        static float maxErr = 0;
+        static float minErr = float.MaxValue;
         static float maxErrLong = 1;
         static void Main(string[] args)
         {
@@ -32,8 +33,9 @@ namespace FlagProject
             for (int i = 0; i < memory.Length; i++)
                 memory[i] = 0;
             memoryLong = new List<float>();
-            Renderer.WindowSize = new Int2(Arit.Clamp(pWidth * 2 + 9 + displaySize.X, 20, int.MaxValue),Arit.Clamp(pHeight+4 + pHeight%2, displaySize.Y*2, int.MaxValue));
+            Renderer.WindowSize = new Int2(Arit.Clamp(Arit.Clamp(pWidth * 2, 12, int.MaxValue) + 9 + displaySize.X, 20, int.MaxValue),Arit.Clamp(pHeight+4 + pHeight%2, displaySize.Y*3, int.MaxValue));
             Renderer.AsyncMode = false;
+            Renderer.WindowTitle = "FlagLearning";
             string[] filePaths = Directory.GetFiles("flags");
             Bitmap[] flagBitmaps = new Bitmap[filePaths.Length];
             flagMatrixes = new Vector<double>[filePaths.Length];
@@ -63,26 +65,31 @@ namespace FlagProject
                     }
                 });
             }
-            PiwotBrainLib.BrainCore bc = new PiwotBrainLib.BrainCore("brain_sml_0.txt");
-            //PiwotBrainLib.BrainCore bc = new PiwotBrainLib.BrainCore(pTotal * 3, new int[6] { pTotal * 6, pTotal, pTotal/2, pTotal/8, pTotal, pTotal * 6 }, pTotal * 3);
+            
+            //PiwotBrainLib.BrainCore bc = new PiwotBrainLib.BrainCore(pTotal * 3, new int[5] { 128, 64, 32, 64, 128 }, pTotal * 3);
 
-            bc.StreachLayer(0, 4, 3);
-            bc.StreachLayer(7, 4, 3);
+            PiwotBrainLib.BrainCore bc = new PiwotBrainLib.BrainCore("brain_mid_103.txt");
+            bc.StreachLayer(0, 2, 2, 12, 0.25);
 
+            bc.StreachLayer(6, 2, 2, 12, 0.25);
 
             PiwotDrawingLib.UI.Containers.SimpleFunctionDisplay f = new PiwotDrawingLib.UI.Containers.SimpleFunctionDisplay(
-                pb2.Position + new Int2(pb2.Size.X + 1, 0), displaySize, "Moment Error", PiwotDrawingLib.Misc.Boxes.BoxType.doubled, ErrorFunc);
+                new Int2(Arit.Clamp(pb2.Position.X + pb2.Size.X + 1, 20, int.MaxValue), pb2.Position.Y), displaySize, "Moment Error", PiwotDrawingLib.Misc.Boxes.BoxType.doubled, ErrorFunc);
 
             PiwotDrawingLib.UI.Containers.SimpleFunctionDisplay f2 = new PiwotDrawingLib.UI.Containers.SimpleFunctionDisplay(
-                pb2.Position + new Int2(pb2.Size.X + 1, f.Size.Y), displaySize, "History error", PiwotDrawingLib.Misc.Boxes.BoxType.doubled, ErrorFuncLong);
-            PiwotDrawingLib.Misc.Boxes.DrawBox(PiwotDrawingLib.Misc.Boxes.BoxType.normal, 1, pHeight / 2 + 2, pb2.Position.X + pb2.Size.X - 1, Renderer.WindowSize.Y - (pHeight / 2 + 2));
+                f.Position + new Int2(0, f.Size.Y), displaySize, "Moment error zoom", PiwotDrawingLib.Misc.Boxes.BoxType.doubled, ErrorFunc2);
+
+            PiwotDrawingLib.UI.Containers.SimpleFunctionDisplay f3 = new PiwotDrawingLib.UI.Containers.SimpleFunctionDisplay(
+                f2.Position + new Int2(0, f2.Size.Y), displaySize, "History error", PiwotDrawingLib.Misc.Boxes.BoxType.doubled, ErrorFuncLong);
+
+            PiwotDrawingLib.Misc.Boxes.DrawBox(PiwotDrawingLib.Misc.Boxes.BoxType.normal, 1, pHeight / 2 + 2, f.Position.X - 1, Renderer.WindowSize.Y - (pHeight / 2 + 2));
 
             PiwotBrainLib.LearningBrain learningBrain = new PiwotBrainLib.LearningBrain(bc)
             {
-                ExampleBlockSize = 3,
+                ExampleBlockSize = 1,
                 DataExtractor = RandomData,
-                Accuracy = 100,
-                ErrorMemoryLenght = 100,
+                Accuracy = 100000,
+                ErrorMemoryLenght = 200,
                 Momentum = 0.1,
                 NeuronActivation = new PiwotBrainLib.SechActivation()
             };
@@ -97,7 +104,7 @@ namespace FlagProject
             while (true)
             {
                 stopwatch.Restart();
-                learningBrain.LearnBlocks(50);
+                learningBrain.LearnBlocks(100);
                 stopwatch.Stop();
                 lTime = (uint)stopwatch.ElapsedMilliseconds;
                 
@@ -117,26 +124,32 @@ namespace FlagProject
                     System.Console.ReadKey(true);
                 }
                 memoryPoint++;
-                memoryPoint %= displaySize.X-2;
+                if (memoryPoint >= displaySize.X - 2)
+                    memoryPoint = 0;
                 GetMaxErr();
                 Renderer.Draw($"AvgErr: {learningBrain.MeanSquaredError.ToString("0.0000")} -", 2, pHeight / 2 + 3);
                 Renderer.Draw($"MaxErr: {maxErr.ToString("0.0000")} -", 2, pHeight / 2 + 4);
-                Renderer.Draw($"Learned: {learningBrain.ExamplesDone} -", 2, pHeight / 2 + 5);
-                Renderer.Draw($"Learn T: {lTime} -", 2, pHeight / 2 + 6);
-                Renderer.Draw($"Calc T: {cTime} -", 2, pHeight / 2 + 7);
-                Renderer.Draw($"Acc: {learningBrain.Accuracy} -", 2, pHeight / 2 + 8);
-                Renderer.Draw($"Mom: {learningBrain.Momentum} -", 2, pHeight / 2 + 9);
+                Renderer.Draw($"MinErr: {minErr.ToString("0.0000")} -", 2, pHeight / 2 + 5);
+                Renderer.Draw($"Max-Min: {(maxErr - minErr).ToString("0.0000")} -", 2, pHeight / 2 + 6);
+                Renderer.Draw($"HMaxErr: {maxErrLong.ToString("0.0000")} -", 2, pHeight / 2 + 7);
+                Renderer.Draw($"Learned: {learningBrain.ExamplesDone} -", 2, pHeight / 2 + 8);
+                Renderer.Draw($"Learn T: {lTime} -", 2, pHeight / 2 + 9);
+                Renderer.Draw($"Calc T: {cTime} -", 2, pHeight / 2 + 10);
+                Renderer.Draw($"Acc: {learningBrain.Accuracy} -", 2, pHeight / 2 + 11);
+                Renderer.Draw($"Mom: {learningBrain.Momentum} -", 2, pHeight / 2 + 12);
                 f.Draw();
                 f2.Draw();
+                f3.Draw();
                 f.RefreshContent();
                 f2.RefreshContent();
+                f3.RefreshContent();
                 Renderer.ForcePrint();
                 times++;
                 if(times % 10 == 0)
                 {
                     memoryLong.Add((float)learningBrain.MeanSquaredError);
                 }
-                if (times > 400)
+                if (times > 100)
                 {
                     times = 0;
                     learningBrain.SaveToFile("", $"brain_mid_{saved}");
@@ -159,6 +172,13 @@ namespace FlagProject
                     x = memory[i];
             }
             maxErr = x;
+            x = float.MaxValue;
+            for (int i = 0; i < memory.Length; i++)
+            {
+                if (x > memory[i])
+                    x = memory[i];
+            }
+            minErr = x;
 
             x = 0;
             for (int i = 0; i < memoryLong.Count; i++)
@@ -170,12 +190,22 @@ namespace FlagProject
         }
         static float ErrorFunc(float x)
         {
+            
+            int id = (int)System.Math.Round(x * memory.Length);
 
-            int id = (int)(x * memory.Length);
+            id += memoryPoint;
+            return ((memory[id % memory.Length])) / (maxErr);
+
+        }
+
+        static float ErrorFunc2(float x)
+        {
+
+            int id = (int)System.Math.Round(x * memory.Length);
 
             id += memoryPoint;
 
-            return memory[id % memory.Length] / maxErr;
+            return ((memory[id % memory.Length]) - minErr) / (maxErr - minErr);
 
         }
 
@@ -183,10 +213,10 @@ namespace FlagProject
         {
             if (memoryLong.Count == 0)
                 return 0;
-            int id = (int)(x * memoryLong.Count);
+            int id = (int)System.Math.Floor(x * memoryLong.Count);
 
 
-            return memoryLong[id % memoryLong.Count] / maxErrLong;
+            return memoryLong[id] / maxErrLong;
 
         }
 
@@ -206,6 +236,7 @@ namespace FlagProject
 
         public static Vector<double> RandomFlag()
         {
+            //return Vector<double>.Build.Dense(pTotal * 3, (r) => ((r * 2) / (pWidth * 3) == 0 ? 1 : 0));
             return flagMatrixes[Rand.Int(flagMatrixes.Length)];
         }
         public static (Matrix<double>, Matrix<double>) RandomData(PiwotBrainLib.LearningBrain lb)
